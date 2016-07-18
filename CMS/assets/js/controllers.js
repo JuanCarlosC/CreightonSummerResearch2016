e8fd0679ba0446772aca4a48ef0ea3bdbce96680
juancarlosc <jchave26@lion.lmu.edu>
2016-07-18 09:35:15 -0500

    adding different views inside modules for front end. Will be working on functionality and serving over the data to a database and vice versa

--- CMS/assets/js/controllers.js
+++ CMS/assets/js/controllers.js
@@ -11,8 +11,56 @@ iCreighton PRODUCT CONTROLLERS
 *******************************************************************************************/
 var cuAppManager = angular.module('cuAppManager', []);
 
-cuAppManager.controller('cuModuleManager', ['$scope', 
-	function($scope) {
-		$scope.yo = "aldjsaljdjklas"
 
+cuAppManager.controller('cuEventManager', ['$scope', '$routeParams', '$rootScope',
+	function($scope, $rootScope, $routeParams) {
+		$scope.title = "Event Module"
+		$scope.featureView = 'data';
+		$("[name='event-module-switch']").bootstrapSwitch();
+		
+	}]);
+
+
+cuAppManager.controller('cuModuleManager', ['$scope','$rootScope', '$routeParams',
+	function($scope, $rootScope, $routeParams) {
+		$scope.title = "General"
+		$scope.featureView = 'data';
+
+		$.fn.bootstrapSwitch.defaults.onColor = 'success';
+	}]);
+
+cuAppManager.controller('cuOverviewManager', ['$scope', '$routeParams', '$rootScope',
+	function($scope, $rootScope, $routeParams) {
+		$scope.title = "Overview"
+		$scope.featureView = 'data';
+	}]);
+cuAppManager.controller('cuAnalyticsManager', ['$scope', '$routeParams', '$rootScope',
+	function($scope, $rootScope, $routeParams) {
+		$scope.title = "Analytics Module";
+		$scope.featureView = 'data';
+	}]);
+cuAppManager.controller('cuSportManager', ['$scope', '$routeParams', '$rootScope',
+	function($scope, $rootScope, $routeParams) {
+		$scope.title = "Sport Module";
+		$scope.featureView = 'data';
+	}]);
+cuAppManager.controller('cuMapManager', ['$scope', '$routeParams', '$rootScope',
+	function($scope, $rootScope, $routeParams) {
+		$scope.title = "Map Module";
+		$scope.featureView = 'data';
+	}]);
+cuAppManager.controller('cuScheduleManager', ['$scope', '$routeParams', '$rootScope',
+	function($scope, $rootScope, $routeParams) {
+		$scope.title = "Schedule Module";
+		$scope.featureView = 'data';
+	}]);
+cuAppManager.controller('cuUsersManager', ['$scope', '$routeParams', '$rootScope',
+	function($scope, $rootScope, $routeParams) {
+		$scope.title = "Users Module";
+		$scope.featureView = 'data';
+	}]);
+cuAppManager.controller('cuStyleManager', ['$scope', '$routeParams', '$rootScope',
+	function($scope, $rootScope, $routeParams) {
+		$scope.title = "Style Module";
+		$scope.featureView = 'data';
 	}]);
--- CMS/assets/js/cuApp.js
+++ CMS/assets/js/cuApp.js
@@ -8,9 +8,45 @@ var cuApp = angular.module('cuApp', [
 cuApp.config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider.
-			when('/MioSimpleDesigner/edit/:orderId', {
-				templateUrl: '../assets/partials/mio-design-edit.html',
-				controller: 'mioDesignerEditCtrl'
+			when('/', {
+				templateUrl: 'assets/js/templates/overview.html',
+				controller: 'cuOverviewManager',
+				animation: 'third'
+			}).
+			when('/module/analytics', {
+				templateUrl: 'assets/js/templates/analytics.html',
+				controller: 'cuAnalyticsManager',
+				animation: 'third'
+			}).
+			when('/module/events', {
+				templateUrl: 'assets/js/templates/events.html',
+				controller: 'cuEventManager',
+				animation: 'third'
+			}).
+			when('/module/sports', {
+				templateUrl: 'assets/js/templates/sports.html',
+				controller: 'cuSportManager',
+				animation: 'third'
+			}).
+			when('/module/maps', {
+				templateUrl: 'assets/js/templates/maps.html',
+				controller: 'cuMapManager',
+				animation: 'third'
+			}).
+			when('/module/schedule', {
+				templateUrl: 'assets/js/templates/schedule.html',
+				controller: 'cuScheduleManager',
+				animation: 'third'
+			}).
+			when('/module/users', {
+				templateUrl: 'assets/js/templates/users.html',
+				controller: 'cuUsersManager',
+				animation: 'third'
+			}).
+			when('/module/styles', {
+				templateUrl: 'assets/js/templates/styles.html',
+				controller: 'cuStyleManager',
+				animation: 'third'
 			}).
 			otherwise({
 				redirectTo:'/'
